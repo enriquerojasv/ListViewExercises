53d9f1be6d21220f7caf8db9e0036e8b8aae0a5c
erojasv <erojasv@cifpfbmoll.eu>
2022-01-29 17:49:59 +0100

    Version 2 with Xs

--- app/src/main/java/com/androidclase/listviewexercises/MainActivity.java
+++ app/src/main/java/com/androidclase/listviewexercises/MainActivity.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.ListActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -18,18 +20,34 @@ public class MainActivity extends ListActivity {
             "etiam", "vel", "erat", "placerat", "ante",
             "porttitor", "sodales", "pellentesque", "augue", "purus"};
 
-
     @Override
     protected void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         setContentView(R.layout.activity_main);
-        setListAdapter(new ArrayAdapter<String>(this, R.layout.row, R.id.label,
-                items));
+        setListAdapter(new IconicAdapter());
         selection = (TextView) findViewById(R.id.selection);
     }
 
     public void onListItemClick(ListView parent, View v, int position, long id) {
         selection.setText(items[position]);
+    }
+
+    class IconicAdapter extends ArrayAdapter<String> {
+        IconicAdapter() {
+            super(MainActivity.this, R.layout.row, R.id.label, items);
+        }
+
+        public View getView(int position, View convertView,
+                            ViewGroup parent) {
+            View row = super.getView(position, convertView, parent);
+            ImageView icon = (ImageView) row.findViewById(R.id.icon);
+            if (items[position].length() > 4) {
+                icon.setImageResource(R.drawable.x);
+            } else {
+                icon.setImageResource(R.drawable.o);
+            }
+            return (row);
+        }
     }
 
 
